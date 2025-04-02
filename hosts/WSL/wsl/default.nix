c1a558f70177de688605d4fd0e7fa136d20d1319
adjoly <adamjly@proton.me>
2025-04-02 10:36:26 +0200

    「🏗️」 wip: work in progress, not done yet.

--- hosts/WSL/wsl/default.nix
+++ hosts/WSL/wsl/default.nix
@@ -5,7 +5,7 @@
 		enable = true;
 		defaultUser = "adjoly";
 		docker-desktop.enable = true;
-		nativeSystemd = true;
+		#		nativeSystemd = true;
 		startMenuLaunchers = true;
 		wslConf.network.hostname = "wsl-adjoly";
 	};
