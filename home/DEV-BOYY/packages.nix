0bae821bc04325d5efaa6e71b8edbdd4ebf4ba7f
adjoly <contact@adjoly.fr>
2025-06-15 20:17:28 +0200

    「🔨」 fix: fixed brightness ctl not being in the good folder

--- home/DEV-BOYY/packages.nix
+++ home/DEV-BOYY/packages.nix
@@ -9,7 +9,6 @@
     postman
     gearlever # for beeper cause the one from the nixpkgs seems bugged
     strawberry # for itunes lib and need to test if upload to ipod works
-    brightnessctl
     self.packages.${pkgs.system}.adjust-brightness # for adjusting brightness on ddc/ci screen
   ];
 }
--- home/LAPTOP-5530/packages.nix
+++ home/LAPTOP-5530/packages.nix
@@ -8,6 +8,7 @@
   home.packages = with pkgs; [
     gearlever # for beeper cause the one from the nixpkgs seems bugged
     # strawberry # for itunes lib and need to test if upload to ipod works
-		self.packages.${pkgs.system}.adjust-brightness
+    self.packages.${pkgs.system}.adjust-brightness
+    brightnessctl
   ];
 }
