023e2064b73665583552514ef0df97b00f6c3ce6
adjoly <contact@adjoly.fr>
2025-06-16 20:02:32 +0200

    「🔨」 fix: fixed nextcloud module

--- modules/home-manager/gui/nextcloud.nix
+++ modules/home-manager/gui/nextcloud.nix
@@ -11,6 +11,9 @@
   };
 
   config = lib.mkIf config.keyzox.nextcloud.enable {
+    home.packages = with pkgs; [
+      nextcloud-client
+    ];
     services.nextcloud-client = {
       enable = true;
       startInBackground = true;
