4ed5f2c5c79b7e25831118af6d78d2f3f84b7891
adjoly <contact@adjoly.fr>
2025-10-18 14:19:14 +0200

    「🆙」 update(nix-direnv): just made a quick update !

--- modules/home-manager/cli/shell/direnv.nix
+++ modules/home-manager/cli/shell/direnv.nix
@@ -10,7 +10,10 @@
   programs.direnv = lib.mkIf config.keyzox.shell.enable {
     enable = lib.mkDefault true;
     enableZshIntegration = true;
-    nix-direnv.enable = true;
+    nix-direnv = {
+		enable = true;
+		package = inputs.unstablepkgs.legacyPackages.${pkgs.system}.nix-direnv;
+	};
     silent = true;
     package = inputs.unstablepkgs.legacyPackages.${pkgs.system}.direnv;
   };
