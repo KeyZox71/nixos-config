f2ce42381f08fdf1aab913fd1a90a0b1be7c83d0
adjoly <contact@adjoly.fr>
2025-10-29 19:23:46 +0100

    「🔨」 fix: now using github to fetch wallpaper

--- modules/home-manager/gui/wm/hyprpaper.nix
+++ modules/home-manager/gui/wm/hyprpaper.nix
@@ -7,8 +7,8 @@
 
 let
   source = pkgs.fetchurl {
-    url = "https://drive.kanel.ovh/s/e9Hqxi85gsSoZg5/download/DSC_4763.jpg";
-    sha256 = "0s4qavjcs9kf1g8vllqlb43i7si9vk6b9338rbpkqc87k6a794hi";
+    url = "https://raw.githubusercontent.com/KeyZox71/nixos-config/refs/heads/master/assets/DSC_4763.jpg";
+    sha256 = "EZJ0lJkHMTzvymiMtMzcKeoTB1kUU7rRC24mzeRWmGg=";
   };
 in
 {
